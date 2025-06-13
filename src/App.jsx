156d4b0bb1d1f760b9d158bee548c28f5a8db385
CJcode6754 <ceejayibabiosa@gmail.com>
2025-06-13 17:05:07 +0800

    feat: add links

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,17 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
-import HomePage from "./components/HomePage";
+import HomePage from "./Pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
-const NotFound = () => (
-  <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white p-4">
-    <div className="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-xl text-center">
-      <p className="text-red-500 text-2xl font-bold">Page Not Found!</p>
-    </div>
-  </div>
-);
+import JobsPage from "./Pages/JobsPage";
+import NotFoundPage from "./Pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage/>}/>
+      <Route path="*" element={<NotFoundPage/>}/>
     </Route>
   )
 );
